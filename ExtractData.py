2574be9dbb10a7af41585798caa889d9f1200bc6
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-04-11 01:24:50 +0700

    fixing python code for the excel output

--- ExtractData.py
+++ ExtractData.py
@@ -2,15 +2,12 @@ import os
 # import ini biar ga banyak warning muncul pas di run
 import warnings
 warnings.filterwarnings('ignore')
-
 import pandas as pd
-# import en_core_web_sm
-# nlp = en_core_web_sm.load()
-# import PyPDF2
 
+# library utk parse CV
 from pyresparser import ResumeParser
 
-# Folder path containing the CVs
+# path ke folder cv
 cv_folder_path = "D:\\UiPath\\ExtractData_V2\\ExtractData_V2\\attachments\\"
 cv_files = os.listdir(cv_folder_path)
 
@@ -18,19 +15,15 @@ processed_data = []
 for cv_file in cv_files:
     data = ResumeParser(os.path.join(cv_folder_path, cv_file)).get_extracted_data()
     cv_data = {
-        "File": data.get("name", ""),
-        "Personal Info": {
-            "Name": data.get("name", ""),
-            "Title": data.get("title", ""),
-            "Location":  data.get("location", ""),
-            "Phone": data.get("mobile_number", ""),
-            "Email": data.get("email", "")
-            
-        },
-    "Summary": data.get("summary", ""),
-    "Experience": data.get("experience", []),
-    "Education": data.get("education",[]),
-    "Skills": data.get("skills",[])
+        "Name": data.get("name", ""),
+        "Email": data.get("email", ""),
+        "Phone Number": data.get("mobile_number", ""),
+        "Position": data.get("designation", ""),
+        "Degree": data.get("degree", ""),
+        "Company Names": data.get("company_names", ""),
+        "Skills": ", ".join(data.get("skills", [])),  # Convert skills list to comma-separated string
+        "Total Experience": data.get("total_experience", ""),
+        "no_of_pages": data.get("no_of_pages", "")
     }
     processed_data.append(cv_data)
 # print(processed_data)
@@ -52,7 +45,18 @@ if os.path.exists(output_excel_path):
         if not os.path.exists(output_excel_path):
             break
         counter += 1
-df.to_excel(output_excel_path, index=False, engine='xlsxwriter')
+        
 
-# Open the Excel file after it's created
+# memasukkan dataframe ke excel
+with pd.ExcelWriter(output_excel_path, engine='xlsxwriter') as writer:
+    df.to_excel(writer, index=False)
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+
+    # Enable text wrapping
+    for i, col in enumerate(df.columns):
+        wrap_format = workbook.add_format({'text_wrap': True})
+        worksheet.set_column(i, i, width=40, cell_format=wrap_format)
+
+# buka file excel
 os.startfile(output_excel_path)
